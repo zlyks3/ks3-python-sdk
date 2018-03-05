622e1971bc418eda588cf999dca6fd346f6820c8
lichenji <lichenji@kingsoft.com>
2018-03-05 17:51:58 +0800

    发布至pypi

--- setup.py
+++ setup.py
@@ -1,12 +1,29 @@
-import os
-from setuptools import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+with open('README.md', 'rb') as f:
+    readme = f.read().decode('utf-8')
 
 setup(
-    name = "ks3sdk",
-    version = "1.0.6",
-    author = "ksc",
-    description = (""),
-    license = "BSD",
-    #url = "http://packages.python.org/an_example_pypi_project",
-    packages=['ks3']
+    name='ks3sdk',
+    version='1.0.6',
+    description='Kingsoft Standard Storage Service SDK',
+    long_description=readme,
+    packages=['ks3'],
+    install_requires=['six'],
+    include_package_data=True,
+    url='https://github.com/ks3sdk/ks3-python-sdk',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7'
+    ]
 )
